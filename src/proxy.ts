030e4e3e3619b49b6d8e4145c357bcf534a3da81
MananVelani <153758822+MananVelani@users.noreply.github.com>
2025-12-26 15:39:25 +0530

    1st try fixing auth

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,39 +1,30 @@
 import { NextResponse, NextRequest } from 'next/server'
-import { getServerSession } from "next-auth";
-import { authOptions } from "./app/api/auth/[...nextauth]/options";
+import { getToken } from "next-auth/jwt"
 
+export default async function middleware(request: NextRequest) {
+  const token = await getToken({ req: request }) // THIS WORKS IN MIDDLEWARE
+  const url = request.nextUrl
 
- 
+  if (token && (
+    url.pathname.startsWith('/sign-in') ||
+    url.pathname.startsWith('/sign-up') ||
+    url.pathname.startsWith('/verify')
+  )) {
+    return NextResponse.redirect(new URL('/dashboard', request.url))
+  }
 
-export default async function proxy(request: NextRequest) {
-
-    const session = await getServerSession(authOptions);
-    const token = session?.user;
-    const url = request.nextUrl;
-
-    if(token && 
-    (
-        url.pathname.startsWith('/sign-in') ||
-        url.pathname.startsWith('/sign-up') ||
-        url.pathname.startsWith('/verify') 
-          )
-    ){
-        return NextResponse.redirect(new URL('/dashboard', request.url))
-    }
-
-    if(!token && ( 
-      url.pathname.startsWith('/dashboard') ||
-      url.pathname.startsWith('/category') ||
-      url.pathname.startsWith('/listing') ||
-      url.pathname.startsWith('/post')
-    )){
-        return NextResponse.redirect(new URL('/sign-in', request.url));
-    }
-
+  if (!token && (
+    url.pathname.startsWith('/dashboard') ||
+    url.pathname.startsWith('/category') ||
+    url.pathname.startsWith('/listing') ||
+    url.pathname.startsWith('/post')
+  )) {
+    return NextResponse.redirect(new URL('/sign-in', request.url))
+  }
 
   return NextResponse.next()
 }
- 
+
 export const config = {
   matcher: [
     '/sign-in',
@@ -42,7 +33,6 @@ export const config = {
     '/verify/:path*',
     '/category/:path*',
     '/listing/:path*',
-    '/post/:post*'
+    '/post/:path*'
   ]
 }
-
